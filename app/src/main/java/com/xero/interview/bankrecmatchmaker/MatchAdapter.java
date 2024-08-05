3387c68b469e30755f98726914c8baa8ecc9ea9d
Daniel <daniel@dcoded.co.nz>
2024-08-05 23:20:11 +1200

    Improvements for base code

--- app/src/main/java/com/xero/interview/bankrecmatchmaker/MatchAdapter.java
+++ app/src/main/java/com/xero/interview/bankrecmatchmaker/MatchAdapter.java
@@ -5,20 +5,45 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.Locale;
 
 public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> {
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    private final List<MatchItem> matchItems;
 
-        private TextView mainText;
-        private TextView total;
-        private TextView subtextLeft;
-        private TextView subtextRight;
+    public MatchAdapter(List<MatchItem> matchItems) {
+        this.matchItems = matchItems;
+    }
 
-        public ViewHolder(View itemView) {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_match, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(matchItems.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return matchItems.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView mainText;
+        private final TextView total;
+        private final TextView subtextLeft;
+        private final TextView subtextRight;
+
+        public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mainText = itemView.findViewById(R.id.text_main);
             total = itemView.findViewById(R.id.text_total);
@@ -28,35 +53,9 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> 
 
         public void bind(MatchItem matchItem) {
             mainText.setText(matchItem.getPaidTo());
-            total.setText(Float.toString(matchItem.getTotal()));
+            total.setText(String.format(Locale.getDefault(), "%.2f", matchItem.getTotal()));
             subtextLeft.setText(matchItem.getTransactionDate());
             subtextRight.setText(matchItem.getDocType());
         }
-
     }
-
-    private List<MatchItem> matchItems;
-
-    public MatchAdapter(List<MatchItem> matchItems) {
-        this.matchItems = matchItems;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        CheckedListItem listItem = (CheckedListItem) layoutInflater.inflate(R.layout.list_item_match, parent, false);
-        return new ViewHolder(listItem);
-    }
-
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        MatchItem matchItem = matchItems.get(position);
-        holder.bind(matchItem);
-    }
-
-    @Override
-    public int getItemCount() {
-        return matchItems.size();
-    }
-
 }
